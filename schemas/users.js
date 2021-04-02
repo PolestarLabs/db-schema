c5e1e1ee1998b1737f9b8615427acee8e61a225c
Tsumari <lucasflicky@hotmail.com>
2021-04-02 02:40:16 -0300

    remove debug

--- schemas/users.js
+++ schemas/users.js
@@ -326,7 +326,6 @@ MODEL.get = function (query, project, avoidNew) {
     }
     if (!typeof project) project = { _id: 0 };
     const data = await this.findOne(query, project).lean();
-    console.log(query)
     if (data === null)  return resolve( this.new( await PLX.resolveUser?.(query.id||query) ) );
 
     return resolve(data);
