3361a3f82890886b764d42a3934458cedbdf27ff
Tsumari <lucasflicky@hotmail.com>
2021-04-19 19:48:43 -0300

    featured marriage ref to relationships

--- schemas/users.js
+++ schemas/users.js
@@ -38,7 +38,7 @@ const UserSchema = new mongoose.Schema({
   eventData: Mixed,
 
   // MODULES
-  featuredMarriage: String,
+  featuredMarriage: {type:String, ref: 'Relationship'},
   counters: Mixed,
   modules: {
     powerups: Mixed,
